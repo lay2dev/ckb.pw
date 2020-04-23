c54c3f046b3fe3dd200d05f1e7997a5115690de3
zhang <zhanghuaqiang@gmail.com>
2020-04-23 19:32:07 +0800

    fix(core): Add ImToken to EIP712Supported platform

--- src/services/ckb/core.js
+++ src/services/ckb/core.js
@@ -74,7 +74,7 @@ export const setFeeRate = _feeRate => {
   )
 }
 export const setPlatform = ({ provider, chain }) => {
-  const EIP712Supported = ['MetaMask']
+  const EIP712Supported = ['MetaMask', 'ImToken']
   platform.provider = provider
   platform.chain = chain
   platform.typedData = EIP712Supported.includes(provider)
